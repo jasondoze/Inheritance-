fd3fe841918339a0d3d729410b57fa72020312b4
La'Tonia Mertica <76631992+LaTonia-Mertica@users.noreply.github.com>
2021-12-18 10:50:34 -0500

    restart

--- inheritance_project/admin.py
+++
@@ -1,7 +0,0 @@
-# Register your models here.
-from django.contrib import admin
-from .models import Artifact
-
-# Registers Artifact model to help guide website functionality
-admin.site.register(Artifact)
-

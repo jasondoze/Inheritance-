41041ec0b6e8fb43fe08892898325452bed41e77
La'Tonia Mertica <latoniamertica@gmail.com>
2021-12-18 15:35:18 -0500

    added amp styles - still need to wire landing and gallery pages per initial intent

--- inheritance/inheritanceapp/forms.py
+++ inheritance/inheritanceapp/forms.py
@@ -1,10 +1,12 @@
 
 from django import forms
+from django.forms import widgets
 from .models import *
 
 # added editor form to these attributes
 class EditorForm(forms.Form):
-    title = forms.CharField(max_length=13, required=True)
-    description = forms.CharField(widget=forms.Textarea, required=True)
-    img_link = forms.URLField(required=True)
-    
+    title = forms.CharField(max_length=13, required=True, label=False, widget=forms.TextInput(attrs={'placeholder': 'enter title'}))
+
+    description = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'add description'}), label=False, required=True)
+
+    img_link = forms.URLField(required=True, label=False, widget=forms.TextInput(attrs={'placeholder': 'enter image link'}))

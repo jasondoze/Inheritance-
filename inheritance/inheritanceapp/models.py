a651e37084543dcfb15ede734cbedb50a48d118c
La'Tonia Mertica <latoniamertica@gmail.com>
2021-12-18 12:20:17 -0500

    initial commit for project restart

---
+++ inheritance/inheritanceapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.db.models.base import *
+
+# Create your models here.
+# model(s) for blog-style additions and edits
+class Artifact(models.Model):
+    artifact_id = models.AutoField(primary_key=True)
+    img_link = models.URLField()
+    title = models.CharField(max_length=13)
+    description = models.TextField(max_length=444)
+
+
---
+++ inheritance/inheritanceapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+# sets the urls for pages on the site
+urlpatterns = [
+    path('', views.landingpage, name='landingpage'),
+    path('ourstory', views.ourstory, name='ourstory'),
+    path('addartifact/', views.addartifact, name='addartifact'),
+    path('gallery', views.gallery, name='gallery'),
+    path('editartifact/<int:artifact_id>', views.editartifact, name="editartifact")
+]
+

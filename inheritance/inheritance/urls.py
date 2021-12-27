a752ec838070812bc29a5fcc739d739612bb2149
Jason <90075907+Jason-Doze@users.noreply.github.com>
2021-12-27 17:55:01 -0600

    Adding code comments throughout project

--- inheritance/inheritance/urls.py
+++ inheritance/inheritance/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-
+# root django app url patterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('inheritanceapp.urls')),

862a79eb96ab330ca3d186473692c917850fbc2e
La'Tonia Mertica <latoniamertica@gmail.com>
2022-01-03 21:56:33 -0500

    corrected active text input bkgrd color, lead spacing, and added placeholders

--- inheritance/inheritanceapp/forms.py
+++ inheritance/inheritanceapp/forms.py
@@ -7,6 +7,10 @@ from .models import *
 # The code has the following structure: The first line is the class definition, which defines what kind of object this is. In this case, it's a model form for the Artifact model. The second line is the class Meta definition, which contains information about how to use this class in our application. This includes defining what type of object it represents and its fields (the list of attributes). This code starts with a class definition for the ImageForm. The next line is an import statement that imports the forms module from django-forms. This allows us to use all of the functions in this module to create our form objects. Next we define a Meta class which defines some attributes about our model (Artifact). We also define an attribute called model which will be set to Artifact when we instantiate it later on in our code. We then define a field called imgtitle which is required and must have a value assigned to it before we can save any data into our database table using this form object. Finally, we declare three other fields: imgdesc, image, and artifact_id as being required fields that need values assigned before saving anything into the database table using this form object.
 class ImageForm(forms.ModelForm):
     """Form for the image model"""
+
+    imgtitle = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "  enter title", "class": "control-the-form"}))
+    imgdesc = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "  enter description", "class": "control-the-form"}))
+    image = forms.FileField(widget=forms.ClearableFileInput(attrs={"class": "control-choosefile-btn"})) 
     class Meta:
         model = Artifact
         fields = ('artifact_id', 'imgtitle', 'imgdesc', 'image')

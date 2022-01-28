39a7e33de088ddb2f314c7c3913de1cb06f3f3e9
La'Tonia Mertica <latoniamertica@gmail.com>
2022-01-28 13:35:39 -0500

    debub bkgrd

--- inheritance/inheritanceapp/views.py
+++ inheritance/inheritanceapp/views.py
@@ -13,7 +13,10 @@ from django.http import HttpResponseBadRequest
 # This function takes in a request and returns the template landingpage.html with an object of all artifacts ordered by their artifact_id. The code is using the objects method to get all artifacts from the database, then it orders them by their artifact_id which will be used later on when rendering the page. The code returns a landingpage.html template that contains the list of all artifacts. 
 def landingpage(request):
     artifact = Artifact.objects.all().order_by('artifact_id')
-    background = random.choice(artifact)
+    if not artifact:
+        background = None
+    else: 
+        background = random.choice(artifact)
     return render(request=request, template_name='landingpage.html', context={'background': background, 'id': id}) 
 
 # This function renders the ourstory html  

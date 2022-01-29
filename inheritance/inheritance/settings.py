e5228440847ca63001bc3e8f56330dba6817529e
La'Tonia Mertica <latoniamertica@gmail.com>
2022-01-28 21:41:08 -0500

    fine tuned delete, wired cancel, added main gallery to gallery browse, wrote disclaimer/disclosure for users, deployed website, and general overall code cleaning and styles

--- inheritance/inheritance/settings.py
+++ inheritance/inheritance/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5+j*)tv!$!wzb8t(&xi%kzuv2bd!cs)5tou!hj!1@!pea(#^u*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['inheritance-artifacts.herokuapp.com']
+ALLOWED_HOSTS = ['inheritance-artifacts.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
